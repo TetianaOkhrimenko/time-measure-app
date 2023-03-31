69c52ed7d7eb8ee80d978a270e3454dfb2fb77f0
TetianaOkhrimenko <TANOH@ukr.net>
2023-03-31 18:13:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -175,10 +175,11 @@ document
         break;
     }
 
-    if (selectDays.value === "option-0") {
+    /*if (selectDays.value === "option-0") {
       resultBox.textContent = "Enter a valid date or choose set of days";
     } else {
       resultBox.textContent =
         message || "Enter a valid date or choose set of days";
-    }
+    }*/
+    resultBox.textContent = message;
   });
